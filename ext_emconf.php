4d6187507229ac1ccf891e32e62f257fd3304741
Frans Saris <frans@beech.it>
2013-11-08 15:38:16 +0100

    [TASK] Remove isLoaded(tt_news) check
    When this extension is installed there is no need to
    hide the importers if tt_news isn't installed. When you do
    not want these importers just un-install this extension.
    
    Removed the check from ext_tables.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,6 +34,7 @@ $EM_CONF[$_EXTKEY] = array(
 			'php' => '5.3.0-0.0.0',
 			'extbase' => '',
 			'fluid' => '',
+			'news' => '',
 		),
 		'conflicts' => array(
 		),
--- ext_tables.php
+++ ext_tables.php
@@ -3,14 +3,11 @@ if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-// show tt_news importer only if tt_news is installed
-if (true || \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('tt_news')) {
-	\Tx_News_Utility_ImportJob::register(
-		'BeechIt\\NewsTtnewsimport\\Jobs\\TTNewsNewsImportJob',
-		'LLL:EXT:news_ttnewsimport/Resources/Private/Language/locallang_be.xml:ttnews_importer_title',
-		'');
-	\Tx_News_Utility_ImportJob::register(
-		'BeechIt\\NewsTtnewsimport\\Jobs\\TTNewsCategoryImportJob',
-		'LLL:EXT:news_ttnewsimport/Resources/Private/Language/locallang_be.xml:ttnewscategory_importer_title',
-		'');
-}
+\Tx_News_Utility_ImportJob::register(
+	'BeechIt\\NewsTtnewsimport\\Jobs\\TTNewsNewsImportJob',
+	'LLL:EXT:news_ttnewsimport/Resources/Private/Language/locallang_be.xml:ttnews_importer_title',
+	'');
+\Tx_News_Utility_ImportJob::register(
+	'BeechIt\\NewsTtnewsimport\\Jobs\\TTNewsCategoryImportJob',
+	'LLL:EXT:news_ttnewsimport/Resources/Private/Language/locallang_be.xml:ttnewscategory_importer_title',
+	'');
