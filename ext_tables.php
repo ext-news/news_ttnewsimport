dd07b1d0092e28fd7324900395c03c1487163968
Torben Hansen <derhansen@gmail.com>
2017-03-31 10:37:48 +0200

    [TASK] Change order of tasks in selectbox
    Closes #32

--- ext_tables.php
+++ ext_tables.php
@@ -4,13 +4,13 @@ if (!defined('TYPO3_MODE')) {
 }
 
 \GeorgRinger\News\Utility\ImportJob::register(
-	'BeechIt\\NewsTtnewsimport\\Jobs\\TTNewsNewsImportJob',
-	'LLL:EXT:news_ttnewsimport/Resources/Private/Language/locallang_be.xml:ttnews_importer_title',
-	'');
-\GeorgRinger\News\Utility\ImportJob::register(
 	'BeechIt\\NewsTtnewsimport\\Jobs\\TTNewsCategoryImportJob',
 	'LLL:EXT:news_ttnewsimport/Resources/Private/Language/locallang_be.xml:ttnewscategory_importer_title',
 	'');
+\GeorgRinger\News\Utility\ImportJob::register(
+    'BeechIt\\NewsTtnewsimport\\Jobs\\TTNewsNewsImportJob',
+    'LLL:EXT:news_ttnewsimport/Resources/Private/Language/locallang_be.xml:ttnews_importer_title',
+    '');
 \GeorgRinger\News\Utility\ImportJob::register(
 	'BeechIt\\NewsTtnewsimport\\Jobs\\MblNewseventImportJob',
 	'LLL:EXT:news_ttnewsimport/Resources/Private/Language/locallang_be.xml:mblnewsevent_importer_title',
