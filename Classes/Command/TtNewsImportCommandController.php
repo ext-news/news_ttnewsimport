3c461247a1f0d21feb5943b10b34ccce39398f08
jkummer <jok@jokmac.local>
2019-03-01 16:44:13 +0100

    [TASK] Add Migration for related news. refs #31

--- Classes/Command/TtNewsImportCommandController.php
+++ Classes/Command/TtNewsImportCommandController.php
@@ -3,7 +3,6 @@ namespace BeechIt\NewsTtnewsimport\Command;
 
 use GeorgRinger\News\Jobs\ImportJobInterface;
 use GeorgRinger\News\Utility\ImportJob;
-use TYPO3\CMS\Extbase\Mvc\Controller\CommandController;
 
 /**
  * Controller to import news from tt_news
@@ -32,6 +31,17 @@ class TtNewsImportCommandController extends \GeorgRinger\News\Command\NewsImport
     public function importTtNewsNewsCommand()
     {
         $job = $this->objectManager->get(\BeechIt\NewsTtnewsimport\Jobs\TTNewsNewsImportJob::class);
+        $job->run(0);
+    }
+
+    /**
+     * Import tt_news related news records
+     *
+     * @cli
+     */
+    public function importTtNewsRelatedNewsCommand()
+    {
+        $job = $this->objectManager->get(\BeechIt\NewsTtnewsimport\Jobs\TTNewsRelatedNewsImportJob::class);
         $job->run(0);
     }
 }
